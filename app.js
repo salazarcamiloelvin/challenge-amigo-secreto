fd3322d4b898d00d8e90f046f60c05d7f0438ba9
Elvin-Salazar <salazarcamiloe@gmail.com>
2025-03-01 09:28:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,36 @@ let listaDeAmigos = [];
 
 
 function agregarAmigo(){
-    let amigo = document.getElementById('amigo');
-    if(amigo.value == ''){
+    let nombre = document.getElementById('amigo');
+    if(nombre.value == ''){
         alert('Digite un nombre valido');
+        return;
     }
-    else{
-        listaDeAmigos.push(amigo.value);
+   listaDeAmigos.push(nombre.value);
+   console.log(listaDeAmigos);
+   nombre.focus();
+   nombre.value = '';
+   mostrarAmigos();
+}
+
+function mostrarAmigos(){
+   let amigos = document.getElementById('listaAmigos');
+   amigos.innerHTML = '';
+
+   for(let i = 0; i < listaDeAmigos.length; i++){
+        let item = document.createElement('li');
+        item.textContent = listaDeAmigos[i];
+        amigos.appendChild(item);
+   }
+}
+
+function sortearAmigo(){
+    if(listaDeAmigos.length === 0){
+        alert('No tienes amigos para sortear');
+        return;
     }
 
+    let amigoAleatorio = listaDeAmigos[Math.floor(Math.random() * listaDeAmigos.length)];
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `El amigo ganador es: ${amigoAleatorio}`;
 }
